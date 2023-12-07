a605befa8294b29c0f96f3a968d79eed25fea285
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-07 16:38:48 +0700

    fix: 🐛 remove wrong plugins

--- lua/core/plugins/code/42.lua
+++ lua/core/plugins/code/42.lua
@@ -1,19 +1,9 @@
 return {
 	"hardyrafael17/norminette42.nvim",
 
-	dependencies = {
-		"vinicius507/header42.nvim",
-	},
-
 	lazy = false,
 
 	opts = {
 		runOnSave = true,
-		login = "uolle",
-		email = "uolle@student.42bangkok.com",
-	},
-
-	keys = {
-		{ "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
 	},
 }
