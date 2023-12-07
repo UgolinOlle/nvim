b0d152a79acc10d763feca93c99f425cd0cd5023
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-07 16:38:26 +0700

    feat: 🎸 add new plugin for header correctly

---
+++ lua/core/plugins/code/header.lua
@@ -0,0 +1,12 @@
+return {
+	"Diogo-ss/42-header.nvim",
+
+	opts = {
+		login = "uolle",
+		email = "uolle@student.42bangkok.com",
+	},
+
+	keys = {
+		{ "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
+	},
+}
