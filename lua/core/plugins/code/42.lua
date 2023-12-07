3a254dbea937464352ffeecbcab0c49ef387360e
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-07 16:34:46 +0700

    feat: 🎸 add good header plguin

--- lua/core/plugins/code/42.lua
+++ lua/core/plugins/code/42.lua
@@ -1,13 +1,19 @@
 return {
 	"hardyrafael17/norminette42.nvim",
 
+	dependencies = {
+		"vinicius507/header42.nvim",
+	},
+
 	lazy = false,
 
 	opts = {
 		runOnSave = true,
+		login = "uolle",
+		email = "uolle@student.42bangkok.com",
 	},
 
 	keys = {
-		{ "<LEADER>he", "<CMD>Header42<CR>", desc = "Set official 42 header.", mode = "n" },
+		{ "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
 	},
 }
