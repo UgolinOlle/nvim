c7ec85ba2d9a1cc762c957088ade612aaf0b5282
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-08 18:31:49 +0700

    style: 💄 change theme to monokai

--- lua/core/plugins/ui/github.lua
+++ lua/core/plugins/ui/github.lua
@@ -1,7 +1,7 @@
 return {
 	"projekt0n/github-nvim-theme",
 
-	config = function()
-		vim.cmd("colorscheme github_dark_dimmed")
-	end,
+	-- config = function()
+	-- 	vim.cmd("colorscheme github_dark_dimmed")
+	-- end,
 }
