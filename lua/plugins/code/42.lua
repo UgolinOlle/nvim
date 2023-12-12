7607708610e2a1916b235c321500b86949cbc9cd
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-12 14:02:13 +0700

    feat: 🎸 embed two plugins for 42

--- lua/plugins/code/42.lua
+++ lua/plugins/code/42.lua
@@ -1,9 +1,23 @@
 return {
-	"hardyrafael17/norminette42.nvim",
+	{
+		"hardyrafael17/norminette42.nvim",
 
-	lazy = false,
+		lazy = false,
 
-	opts = {
-		runOnSave = true,
+		opts = {
+			runOnSave = true,
+		},
+	},
+	{
+		"Diogo-ss/42-header.nvim",
+
+		opts = {
+			user = "uolle",
+			mail = "uolle@student.42bangkok.com",
+		},
+
+		keys = {
+			{ "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
+		},
 	},
 }
