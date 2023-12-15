df615212458c414f82ab4b2ce6281029877924ab
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-15 19:37:04 +0700

    refactor: 💡 adding some new options & update way to load opts

--- lua/core/common.lua
+++ lua/core/common.lua
@@ -1,44 +1,53 @@
 local options = {
-	backup = false,
-	clipboard = "unnamedplus",
-	cmdheight = 2,
-	completeopt = { "menuone", "noselect" },
-	fileencoding = "utf-8",
-	hlsearch = true,
-	ignorecase = true,
-	mouse = "a",
-	pumheight = 10,
-	showmode = false,
-	showtabline = 2,
-	smartcase = true,
-	smartindent = true,
-	splitbelow = true,
-	splitright = true,
-	swapfile = false,
-	updatetime = 300,
-	writebackup = false,
-	expandtab = true,
-	shiftwidth = 2,
-	tabstop = 2,
-	cursorline = true,
-	number = true,
-	relativenumber = false,
-	numberwidth = 4,
-	termguicolors = true,
-	signcolumn = "yes",
-	wrap = false,
-	linebreak = true,
-	linespace = 10,
+	opt = {
+		backup = false,
+		clipboard = "unnamedplus",
+		cmdheight = 2,
+		completeopt = { "menuone", "noselect" },
+		fileencoding = "utf-8",
+		hlsearch = true,
+		ignorecase = true,
+		mouse = "a",
+		pumheight = 10,
+		showmode = false,
+		showtabline = 2,
+		smartcase = true,
+		smartindent = true,
+		splitbelow = true,
+		splitright = true,
+		swapfile = false,
+		updatetime = 300,
+		writebackup = false,
+		expandtab = true,
+		shiftwidth = 2,
+		tabstop = 2,
+		cursorline = true,
+		number = true,
+		relativenumber = false,
+		numberwidth = 4,
+		termguicolors = true,
+		signcolumn = "yes",
+		wrap = false,
+		linebreak = true,
+		linespace = 10,
+	},
+
+	g = {
+		icons_enabled = true,
+	},
+
+	o = {
+		autored = true,
+	},
 }
 
-for key, value in pairs(options) do
-	vim.opt[key] = value
+for scope, table in pairs(options) do
+	for key, value in pairs(table) do
+		vim[scope][key] = value
+	end
 end
 
+-- Specific options
 vim.opt.shortmess:append("c")
 vim.opt.iskeyword:append("-")
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")
-
-vim.g.icons_enabled = true
-
-vim.o.autored = true
