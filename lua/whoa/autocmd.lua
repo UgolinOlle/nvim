5a3f9158f9f704d06083762f4b0dded62161e607
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-20 11:58:27 +0100

    feat: 🎸 removing style on term

--- lua/whoa/autocmd.lua
+++ lua/whoa/autocmd.lua
@@ -4,3 +4,11 @@ autocmd("FileType", {
 	pattern = "c",
 	command = "setlocal shiftwidth=4 tabstop=4",
 })
+
+autocmd("TermOpen", {
+	desc = "Disable foldcolumn in terminal and signcolumn for terminal.",
+	callback = function()
+		vim.opt.foldcolumn = "0"
+		vim.opt.signcolumn = "no"
+	end,
+})
