a64c35a19fc3714258e4472a990e8e9c96706b9b
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-22 18:42:50 +0100

    feat: 🎸 adding some ui

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -19,10 +19,22 @@ require("lazy").setup({
 	{ import = "plugins.navigation" },
 	{ import = "plugins.ui" },
 
+	-- UI
+	ui = {
+		border = "double",
+		size = {
+			width = 0.8,
+			height = 0.8,
+		},
+	},
+
+	-- Checking for updates
 	checker = {
 		enabled = true,
 		notify = false,
 	},
+
+	-- Checking for change
 	change_detection = {
 		notify = false,
 	},
