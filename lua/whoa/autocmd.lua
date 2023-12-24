0857dc3fda8fc526cafe8c84469d4f614d985ade
Ugolin Ollé <hello@ugolin-olle.com>
2023-12-24 09:25:33 +0100

    docs: ✏️ adding comment

--- lua/whoa/autocmd.lua
+++ lua/whoa/autocmd.lua
@@ -1,10 +1,16 @@
 local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.api.nvim_command
 
+-- Adding theme (gruvbox).
+vim.cmd([[colorscheme gruvbox]])
+
+-- Adding 42 norm on C file.
 autocmd("FileType", {
 	pattern = "c",
 	command = "setlocal shiftwidth=4 tabstop=4",
 })
 
+-- Disable some style on terminal.
 autocmd("TermOpen", {
 	desc = "Disable foldcolumn in terminal and signcolumn for terminal.",
 	callback = function()
@@ -12,3 +18,5 @@ autocmd("TermOpen", {
 		vim.opt.signcolumn = "no"
 	end,
 })
+
+-- Open symbol-outline on startup.
