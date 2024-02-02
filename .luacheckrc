c895d79e66c02a9990e2bcc9b30e1e5ac267227e
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-02 12:03:42 +0700

    feat(plugins): adding luachecker linter

---
+++ .luacheckrc
@@ -0,0 +1,13 @@
+-- Global objects
+globals = {
+	"astronvim",
+	"astronvim_installation",
+	"vim",
+	"bit",
+}
+
+-- Rerun tests only if their modification time changed
+cache = true
+
+-- Don't report unused self arguments of methods
+self = false
--- lua/plugins/code/linting.lua
+++ lua/plugins/code/linting.lua
@@ -11,21 +11,29 @@ return {
 			return
 		end
 
+		lint.linters.norminette = {
+			cmd = "norminette",
+		}
+
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			python = { "pylint" },
+			lua = { "luacheck" },
 			codespell = { "codespell" },
 			dotenv = { "dotenv_linter" },
+			c = { "norminette" },
 		}
 
 		local lint_autogroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_autogroup,
-			callback = function() end,
+			callback = function()
+				lint.try_lint()
+			end,
 		})
 
 		vim.keymap.set("n", "<LEADER>l", function()
