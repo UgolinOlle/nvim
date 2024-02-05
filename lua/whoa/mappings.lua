690610e39e36a52c661823740e9347052ea2941f
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-05 23:51:01 +0700

    feat(core): new keymaps for boost productivity

--- lua/whoa/mappings.lua
+++ lua/whoa/mappings.lua
@@ -4,37 +4,84 @@ local opts = { silent = true, noremap = true }
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
 
--- NORMAL
--- Save
+-- Common
+opts.desc = "Save without quit"
 keymap("n", "<LEADER>w", "<CMD>confirm w<ENTER>", opts)
-keymap("n", "<LEADER>q", "<CMD>confirm q<ENTER>", opts)
-keymap("n", "<C-s>", "<CMD>confirm w!<ENTER>", opts)
-keymap("n", "<LEADER>d", "<CMD>bd<CR>", opts)
+
+opts.desc = "Close buffer"
+keymap("n", "<LEADER>q", "<CMD>bd<ENTER>", opts)
+
+opts.desc = "Close window"
+keymap("n", "Q", "<CMD>confirm q<CR>", opts)
+
+opts.desc = "Make the current file executable"
+keymap("n", "<LEADER>x", "<CMD>!chmod +x %<CR>", opts)
 
 -- Window navigation
-keymap("n", "<S-h>", "<C-w>h", opts)
-keymap("n", "<S-j>", "<C-w>j", opts)
-keymap("n", "<S-k>", "<C-w>k", opts)
-keymap("n", "<S-l>", "<C-w>l", opts)
+opts.desc = "Move on left window"
+keymap("n", "<C-h>", "<C-w>h", opts)
+
+opts.desc = "Move on bottom window"
+keymap("n", "<C-j>", "<C-w>j", opts)
+
+opts.desc = "Move on up window"
+keymap("n", "<C-k>", "<C-w>k", opts)
+
+opts.desc = "Move on right window"
+keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Split screen
+opts.desc = "Split horizontal screen"
 keymap("n", "ss", "<CMD>split<CR>", opts)
+
+opts.desc = "Split vertical screen"
 keymap("n", "sv", "<CMD>vsplit<CR>", opts)
 
 -- Tab navigation
+opts.desc = "New tab"
 keymap("n", "<S-t>", "<CMD>tabnew<CR>", opts)
+
+opts.desc = "Go to next tab"
 keymap("n", "<Tab>", "<CMD>BufferLineCycleNext<CR>", opts)
+
+opts.desc = "Go to previous tab"
 keymap("n", "<S-Tab>", "<CMD>BufferLineCyclePrev<CR>", opts)
 
 -- Resize with arrows
+opts.desc = "Decrease height"
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+
+opts.desc = "Increase height"
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+
+opts.desc = "Decrease width"
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+
+opts.desc = "Increase width"
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", ":m .+1<CR>==", opts)
-keymap("n", "<A-k>", ":m .-2<CR>==", opts)
+opts.desc = "Move text down"
+keymap("v", "<S-j>", ":m '>+1<CR>gv=gv", opts)
+
+opts.desc = "Move text up"
+keymap("v", "<S-k>", ":m '<-2<CR>gv=gv", opts)
+
+-- Exit in insert mode
+opts.desc = "Exit insert mode"
+keymap("i", "jk", "<ESC>", opts)
+
+opts.desc = "Exit insert mode"
+keymap("i", "kj", "<ESC>", opts)
+
+-- Indent
+opts.desc = "Indent line"
+keymap("v", "<", "<gv^", opts)
+
+opts.desc = "Unindent line"
+keymap("v", ">", ">gv^", opts)
+
+-- Plugins --
 
 -- Zen mode
 keymap("n", "<LEADER>z", "<CMD>ZenMode<CR>", opts)
@@ -44,28 +91,9 @@ keymap("n", "<LEADER>x", "<CMD>TroubleToggle<CR>", opts)
 keymap("n", "<LEADER>xd", "<CMD>Trouble document_diagnostics<CR>", opts)
 keymap("n", "<LEADER>xw", "<CMD>Trouble workspace_diagnostics<CR>", opts)
 
--- Insert
--- Exit in insert mode
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", "kj", "<ESC>", opts)
-
--- VISUAL
--- Indent
-keymap("v", "<", "<gv^", opts)
-keymap("v", ">", ">gv^", opts)
-
--- VISUAL BLOCK
--- Move text up and down
-keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
-keymap("x", "<A-j>", ":m '>+1<CR>gv=gv", opts)
-keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", opts)
-
--- Plugins --
-
 -- Mason
 opts.desc = "Mason home"
-keymap("n", "<LEADER>pm", "<CMD>Mason<CR>", opts)
+keymap("n", "<LEADER>pp", "<CMD>Mason<CR>", opts)
 
 opts.desc = "Mason clear"
 keymap("n", "<LEADER>pM", "<CMD>MasonUpdate<CR>", opts)
