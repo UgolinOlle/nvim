d22e51c268ba97040ece2f906fdf3e5c8b00810c
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-05 23:48:51 +0700

    perf(plugins): rework handler lsp

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -9,22 +9,56 @@ return {
 	},
 
 	config = function()
-		local lsp_status, lspconfig = pcall(require, "lspconfig")
-		if not lsp_status then
+		-- Check if LSP is available
+		local lsp_active, lspconfig = pcall(require, "lspconfig")
+		if not lsp_active then
 			return
 		end
 
-		local cmp_status, cmp = pcall(require, "cmp_nvim_lsp")
-		if not cmp_status then
+		-- Check if cmp is available
+		local cmp_active, cmp = pcall(require, "cmp_nvim_lsp")
+		if not cmp_active then
 			return
 		end
 
+		-- Variables
 		local utils = require("whoa.utils")
 		local get_icons = utils.get_icons
+
+		-- LSP configuration
+		local capabilities = cmp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+		local signs = {
+			Error = get_icons("DiagnosticError"),
+			Warn = get_icons("DiagnosticWarn"),
+			Hint = get_icons("DiagnosticHint"),
+			Info = get_icons("DiagnosticInfo"),
+		}
+
+		-- Keymaps variables
 		local keymap = vim.keymap
 		local opts = { noremap = true, silent = true }
 
+		-- LSP on_attach function
 		local on_attach = function(_, bufnr)
+			-- Set LSP omnifunc
+			vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+			-- Autocmd for LSP hover
+			vim.api.nvim_create_autocmd("CursorHold", {
+				buffer = bufnr,
+				desc = "Show LSP hover",
+				callback = function()
+					vim.diagnostic.open_float(nil, {
+						focusable = false,
+						close_events = { "BufLeave", "CursorMoved", "FocusLost", "InsertEnter" },
+						border = "rounded",
+						source = "always",
+						prefix = " ",
+					})
+				end,
+			})
+
+			-- Set LSP keymaps
 			opts.buffer = bufnr
 
 			opts.desc = "Show LSP references"
@@ -37,7 +71,7 @@ return {
 			end, opts)
 
 			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<CMD>lua vim.lsp.buf.definitions<CR>", opts)
+			keymap.set("n", "gd", vim.lsp.buf.definitions, opts)
 
 			opts.desc = "Show LSP on hover"
 			keymap("n", "K", vim.lsp.buf.hover, opts)
@@ -58,10 +92,13 @@ return {
 			keymap.set("n", "<LEADER>di", "<CMD>Telescope diagnostics bufnr=0<CR>", opts)
 
 			opts.desc = "Go to previous diagnostics"
-			keymap.set("n", "[d", vim.diagnostics.goto_prev, opts)
+			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
 
 			opts.desc = "Go to next diagnostics"
-			keymap.set("n", "]d", vim.diagnostics.goto_next, opts)
+			keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+			opts.desc = "Show diagnostics in floating window"
+			keymap.set("n", "[w", vim.diagnostic.open_float, opts)
 
 			opts.desc = "Restart LSP"
 			keymap.set("n", "<LEADER>rs", "<CMD>LspRestart<CR>", opts)
@@ -70,19 +107,13 @@ return {
 			keymap.set("n", "<LEADER>li", "<CMD>LspInfo<CR>", opts)
 		end
 
-		local capabilities = cmp.default_capabilities()
-		local signs = {
-			Error = get_icons("DiagnosticError"),
-			Warn = get_icons("DiagnosticWarn"),
-			Hint = get_icons("DiagnosticHint"),
-			Info = get_icons("DiagnosticInfo"),
-		}
-
+		-- Set LSP signs
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 		end
 
+		-- LSP servers configurations
 		local servers = { "pyright", "bashls", "tsserver", "tailwindcss" }
 
 		for _, lsp in ipairs(servers) do
@@ -92,6 +123,7 @@ return {
 			})
 		end
 
+		-- Custom configuration for specific LSPs
 		lspconfig["lua_ls"].setup({
 			capabilities = capabilities,
 			on_attach = on_attach,
@@ -112,5 +144,26 @@ return {
 			end,
 			capabilities = capabilities,
 		})
+
+		-- LSP configuration
+		vim.diagnostic.config({
+			virtual_text = {
+				source = "always",
+				prefix = "●",
+			},
+			underline = true,
+			signs = true,
+			severity_sort = true,
+			float = {
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+				focusable = false,
+			},
+		})
+
+		-- LSP windows configuration
+		require("lspconfig.ui.windows").default_options.border = "rounded"
 	end,
 }
