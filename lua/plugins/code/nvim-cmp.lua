a396bd7bdb87811163763f799c8ecb623940a64b
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-07 23:07:13 +0700

    fix(plugins): switch to opts from lazy & new keymap to try

--- lua/plugins/code/nvim-cmp.lua
+++ lua/plugins/code/nvim-cmp.lua
@@ -4,73 +4,79 @@ return {
 	event = "InsertEnter",
 
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
+		"neovim/nvim-lspconfig", -- LSP client
+		"hrsh7th/cmp-nvim-lsp", -- source for LSP completions
 		"hrsh7th/cmp-buffer", -- source for text in buffer
 		"hrsh7th/cmp-path", -- source for file system paths
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"rafamadriz/friendly-snippets", -- useful snippets
+		"hrsh7th/cmp-cmdline", -- source for command line completions
 		"onsails/lspkind.nvim", -- vs-code like pictograms
 	},
 
-	config = function()
-		local cmp_status, cmp = pcall(require, "cmp")
-		if not cmp_status then
+	opts = function()
+		local cmp_active, cmp = pcall(require, "cmp")
+		if not cmp_active then
 			return
 		end
 
-		local luasnip_status, luasnip = pcall(require, "luasnip")
-		if not luasnip_status then
+		local luasnip_active, luasnip = pcall(require, "luasnip")
+		if not luasnip_active then
 			return
 		end
 
-		local lspkind_status, lspkind = pcall(require, "lspkind")
-		if not lspkind_status then
+		local lspkind_active, lspkind = pcall(require, "lspkind")
+		if not lspkind_active then
 			return
 		end
 
 		require("luasnip.loaders.from_vscode").lazy_load()
 
-		cmp.setup({
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, { { name = "cmdline" } }),
+		})
 
+		cmp.setup({
+			-- Configure sources from which to get completions
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+
+			-- Configure the completion menu
 			completion = {
 				completeopt = "menu,menuone,preview,noselect",
 			},
 
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-
+			-- Configure the completion window
 			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered(),
 			},
 
+			-- Configure the key mappings
 			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-				["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-				["<A-k>"] = cmp.mapping.scroll_docs(-4),
-				["<A-j>"] = cmp.mapping.scroll_docs(4),
+				["<Up>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+				["<Down>"] = cmp.mapping.select_next_item(), -- next suggestion
+				["<C-u>"] = cmp.mapping.scroll_docs(-4),
+				["<C-d>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-
+			-- Configure formatting
 			formatting = {
 				format = lspkind.cmp_format({
-					maxwidth = 50,
+					mode = "symbol",
+					maxwidth = 25,
 					ellipsis_char = "...",
 					menu = {
 						nvim_lsp = "[LSP]",
+						path = "[PATH]",
+						buffer = "[BUF]",
 					},
 				}),
 			},
