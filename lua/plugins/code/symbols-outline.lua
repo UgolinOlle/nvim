5ecc9bc0091d0fe60396b8a72b2fd10a84027518
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-08 15:31:44 +0700

    feat(plugins): adding symbols-outline

---
+++ lua/plugins/code/symbols-outline.lua
@@ -0,0 +1,13 @@
+return {
+	"simrat39/symbols-outline.nvim",
+
+	opts = {},
+
+	keys = {
+		{
+			"<LEADER>ll",
+			"<CMD>SymbolsOutline<CR>",
+			{ noremap = true, silent = true, desc = "Open symbols outline" },
+		},
+	},
+}
