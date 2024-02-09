e6dfdfe6f2ed1e7f585793d386e2135a76c972af
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-09 20:20:17 +0700

    feat(plugins): add new options (mappings)

--- lua/plugins/code/copilot.lua
+++ lua/plugins/code/copilot.lua
@@ -1,3 +1,26 @@
 return {
 	"github/copilot.vim",
+
+	event = "InsertEnter",
+
+	cmd = "Copilot",
+
+	opts = {
+		layout = {
+			position = "bottom",
+			ratio = 0.4,
+		},
+
+		suggestion = {
+			enabled = true,
+
+			keymap = {
+				accept = "<TAB>",
+				accept_line = "<S-TAB>",
+				next = "<C-]>",
+				prev = "<C-[>",
+				dimiss = "<ESC>",
+			},
+		},
+	},
 }
