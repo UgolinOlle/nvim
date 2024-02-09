53212af4121f756bbbb481ff6301ce909d44f38d
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-09 20:20:31 +0700

    feat(plugins): adding new options to git

--- lua/plugins/code/git.lua
+++ lua/plugins/code/git.lua
@@ -1,3 +1,5 @@
+local text = "┃"
+
 return {
 	{
 		"lewis6991/gitsigns.nvim",
@@ -6,18 +8,42 @@ return {
 
 		opts = {
 			signs = {
-				add = { text = "│" },
-				change = { text = "│" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-				untracked = { text = "┆" },
+				add = { hl = "GitSignsAdd", text = text, numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+				change = { hl = "GitSignsChange", text = text, numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+				delete = { hl = "GitSignsDelete", text = text, numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+				topdelete = {
+					hl = "GitSignsDelete",
+					text = text,
+					numhl = "GitSignsDeleteNr",
+					linehl = "GitSignsDeleteLn",
+				},
+				changedelete = {
+					hl = "GitSignsChange",
+					text = text,
+					numhl = "GitSignsChangeNr",
+					linehl = "GitSignsChangeLn",
+				},
+				untracked = { hl = "GitSignsAdd", text = text, numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
 			},
 			signcolumn = true,
-
-			preview_config = {
-				border = "none",
+			numhl = false,
+			linehl = false,
+			word_diff = false,
+			watch_gitdir = {
+				interval = 1000,
+				follow_files = true,
 			},
+			attach_to_untracked = true,
+			current_line_blame = false,
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "right_align",
+				delay = 1000,
+				ignore_whitespace = false,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			sign_priority = 1,
+			update_debounce = 100,
 		},
 	},
 	{
