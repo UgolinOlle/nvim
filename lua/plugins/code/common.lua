15c81d4bcb930d611feb84976c8ae62008527a21
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-09 14:34:37 +0700

    refactor(plugins): move autopairs to common file plugins

--- lua/plugins/code/common.lua
+++ lua/plugins/code/common.lua
@@ -7,6 +7,37 @@ return {
 		version = "*",
 	},
 	{
+		"windwp/nvim-autopairs",
+
+		event = "InsertEnter",
+
+		dependencies = {
+			"hrsh7th/nvim-cmp",
+		},
+
+		opts = {
+			disable_filetype = { "TelescopePrompt" },
+		},
+
+		-- TODO: To remove.
+
+		-- config = function()
+		-- 	local status, autopairs = pcall(require, "nvim-autopairs")
+		-- 	if not status then
+		-- 		return
+		-- 	end
+		--
+		-- 	autopairs.setup({
+		-- 		check_ts = true,
+		-- 	})
+		--
+		-- 	local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+		-- 	local cmp = require("cmp")
+		--
+		-- 	cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		-- end,
+	},
+	{
 		"numToStr/Comment.nvim",
 
 		event = { "BufReadPre", "BufNewFile" },
