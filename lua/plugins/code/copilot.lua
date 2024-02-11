c4a643f77cafe7eb9dfee43fcbf99b6d4a63db38
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-11 21:08:01 +0700

    fix(plugins): copilot need setup

--- lua/plugins/code/copilot.lua
+++ lua/plugins/code/copilot.lua
@@ -5,22 +5,29 @@ return {
 
 	cmd = "Copilot",
 
-	opts = {
-		layout = {
-			position = "bottom",
-			ratio = 0.4,
-		},
+	config = function()
+		local active, copilot = pcall(require, "copilot")
+		if not active then
+			return
+		end
 
-		suggestion = {
-			enabled = true,
-
-			keymap = {
-				accept = "<TAB>",
-				accept_line = "<S-TAB>",
-				next = "<C-]>",
-				prev = "<C-[>",
-				dimiss = "<ESC>",
+		copilot.setup({
+			layout = {
+				position = "bottom",
+				ratio = 0.4,
 			},
-		},
-	},
+
+			suggestion = {
+				enabled = true,
+
+				keymap = {
+					accept = "<TAB>",
+					accept_line = "<S-TAB>",
+					next = "<C-]>",
+					prev = "<C-[>",
+					dimiss = "<ESC>",
+				},
+			},
+		})
+	end,
 }
