7ec7c049bf2feb3f7b5bcb2d26c8ce148e0c2137
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:36:49 +0700

    style(plugins): format code style

--- lua/plugins/code/formatting.lua
+++ lua/plugins/code/formatting.lua
@@ -1,36 +1,36 @@
 return {
-	"stevearc/conform.nvim",
+  "stevearc/conform.nvim",
 
-	lazy = true,
+  lazy = true,
 
-	event = { "BufReadPre", "BufNewFile" },
+  event = { "BufReadPre", "BufNewFile" },
 
-	opts = {
-		formatters_by_ft = {
-			javascript = { "prettier" },
-			typescript = { "prettier" },
-			javascriptreact = { "prettier" },
-			typescriptreact = { "prettier" },
-			json = { "prettier" },
-			yaml = { "prettier" },
-			markdown = { "prettier" },
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			c = { "c_formatter_42" },
-		},
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      lua = { "stylua" },
+      python = { "isort", "black" },
+      c = { "c_formatter_42" },
+    },
 
-		format_on_save = {
-			lsp_fallback = true,
-			async = false,
-			timeout_ms = 1000,
-		},
+    format_on_save = {
+      lsp_fallback = true,
+      async = false,
+      timeout_ms = 1000,
+    },
 
-		formatters = {
-			c_formatter_42 = {
-				command = "c_formatter_42",
-				args = { "$FILENAME" },
-				stdin = false,
-			},
-		},
-	},
+    formatters = {
+      c_formatter_42 = {
+        command = "c_formatter_42",
+        args = { "$FILENAME" },
+        stdin = false,
+      },
+    },
+  },
 }
