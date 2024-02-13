eed1c36fd3e8e30089ed3973434f7bd86c15ba15
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:04:05 +0700

    fix(plugins): remove unused keymap

--- lua/plugins/code/devdocs.lua
+++ lua/plugins/code/devdocs.lua
@@ -1,61 +1,56 @@
 return {
-	"luckasRanarison/nvim-devdocs",
+  "luckasRanarison/nvim-devdocs",
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	},
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
 
-	config = true,
+  config = true,
 
-	opts = {
-		wrap = true,
+  opts = {
+    wrap = true,
 
-		after_open = function(bufnr)
-			vim.api.nvim_buf_set_keymap(bufnr, "n", "<ESC>", "<CMD>close<CR>", { silent = true, noremap = true })
-		end,
+    after_open = function(bufnr)
+      vim.api.nvim_buf_set_keymap(bufnr, "n", "<ESC>", "<CMD>close<CR>", { silent = true, noremap = true })
+    end,
 
-		ensure_installed = {
-			"angular",
-			"bash",
-			"c",
-			"css",
-			"docker",
-			"eslint",
-			"html",
-			"http",
-			"lua",
-			"javascript",
-			"jsdoc",
-			"markdown",
-			"node",
-			"prettier",
-			"sass",
-			"tailwindcss",
-			"typescript",
-		},
-	},
+    ensure_installed = {
+      "angular",
+      "bash",
+      "c",
+      "css",
+      "docker",
+      "eslint",
+      "html",
+      "http",
+      "lua",
+      "javascript",
+      "jsdoc",
+      "markdown",
+      "node",
+      "prettier",
+      "sass",
+      "tailwindcss",
+      "typescript",
+    },
+  },
 
-	keys = {
-		{
-			"<LEADER>v ",
-			"<CMD>DevdocsToggle<CR>",
-			{ noremap = true, silent = true, desc = "Toggle devdocs in floating window." },
-		},
-		{
-			"<LEADER>vi",
-			"<CMD>DevdocsInstall<CR>",
-			{ noremap = true, silent = true, desc = "Install option ensure_installed devdocs." },
-		},
-		{
-			"<LEADER>vv",
-			"<CMD>DevdocsOpenCurrentFloat<CR>",
-			{
-				noremap = true,
-				silent = true,
-				desc = "Open devdocs for file type in a floating window.",
-			},
-		},
-	},
+  keys = {
+    {
+      "<LEADER>vi",
+      "<CMD>DevdocsInstall<CR>",
+      { noremap = true, silent = true, desc = "Install option ensure_installed devdocs." },
+    },
+    {
+      "<LEADER>vv",
+      "<CMD>DevdocsOpenCurrentFloat<CR>",
+      {
+        noremap = true,
+        silent = true,
+        desc = "Open devdocs for file type in a floating window.",
+      },
+    },
+  },
 }
