fdfbe1788830f998a2f0647ae08511ea557cf371
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 14:46:56 +0700

    refactor(plugins): make plugins the main

--- lua/plugins/code/42.lua
+++ lua/plugins/code/42.lua
@@ -1,17 +1,12 @@
 return {
-	{
-		"Diogo-ss/42-header.nvim",
+  "Diogo-ss/42-header.nvim",
 
-		opts = {
-			user = "uolle",
-			mail = "uolle@student.42bangkok.com",
-		},
+  opts = {
+    user = "uolle",
+    mail = "uolle@student.42bangkok.com",
+  },
 
-		keys = {
-			{ "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
-		},
-	},
-	{
-		"cacharle/c_formatter_42.vim",
-	},
+  keys = {
+    { "<LEADER>he", "<CMD>Stdheader<CR>", desc = "Set official 42 header.", mode = "n" },
+  },
 }
