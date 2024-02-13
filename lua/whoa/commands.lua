492aa1bdc8cf1a695d4de71cd82e50a540623178
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:43:27 +0700

    fix(core): change create_command name with w_ prefix

--- lua/whoa/commands.lua
+++ lua/whoa/commands.lua
@@ -1,6 +1,6 @@
 local utils = require "whoa.utils"
 local functions = utils.functions
-local w_update_all, w_create_command = functions.w_update_all, functions.create_command
+local w_update_all, w_create_command = functions.w_update_all, functions.w_create_command
 
 -- Command to update all plugins.
 w_create_command("WhoaUpdateAll", w_update_all)
