e5854008a1545239a86851a59318c4c984f87215
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:39:48 +0700

    feat(plugins): adding file exclude for dashboard

--- lua/plugins/code/indent.lua
+++ lua/plugins/code/indent.lua
@@ -1,80 +1,87 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
+  {
+    "lukas-reineke/indent-blankline.nvim",
 
-		config = function()
-			local active, indent = pcall(require, "ibl")
-			if not active then
-				return
-			end
+    config = function()
+      local active, indent = pcall(require, "ibl")
+      if not active then return end
 
-			local hooks = require("ibl.hooks")
-			local highlight = {
-				"RainbowRed",
-				"RainbowYellow",
-				"RainbowBlue",
-				"RainbowTeal",
-				"RainbowGreen",
-				"RainbowOrange",
-				"RainbowCyan",
-			}
+      local hooks = require "ibl.hooks"
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowTeal",
+        "RainbowGreen",
+        "RainbowOrange",
+        "RainbowCyan",
+      }
 
-			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
-				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
-				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
-				vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
-				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
-				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
-				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
-			end)
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#ea6962" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#fabd2f" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#458588" })
+        vim.api.nvim_set_hl(0, "RainbowTeal", { fg = "#8ec07c" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#b8bb26" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#e78a4e" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#83a598" })
+      end)
 
-			indent.setup({
-				indent = {
-					char = "│",
-					tab_char = "│",
-					highlight = highlight,
-				},
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
-				scope = {
-					enabled = false,
-				},
+      indent.setup {
+        filetype_exclude = {
+          "lazy",
+          "neo-tree",
+          "mason",
+          "dashboard",
+          "terminal",
+          "help",
+        },
 
-				vim.opt.listchars:append("space: "),
-			})
-		end,
-	},
-	{
-		"echasnovski/mini.indentscope",
+        indent = {
+          char = "│",
+          tab_char = "│",
+          highlight = highlight,
+        },
 
-		version = false,
+        scope = {
+          enabled = true,
+        },
 
-		event = { "BufReadPre", "BufNewFile" },
+        vim.opt.listchars:append "space: ",
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
 
-		opts = {
-			symbol = "│",
-			options = { try_as_border = true },
-		},
+    version = false,
 
-		init = function()
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = {
-					"help",
-					"alpha",
-					"hyper",
-					"dashboard",
-					"neo-tree",
-					"Trouble",
-					"lazy",
-					"mason",
-					"notify",
-					"toggleterm",
-					"lazyterm",
-				},
-				callback = function()
-					vim.b.miniindentscope_disable = true
-				end,
-			})
-		end,
-	},
+    event = { "BufReadPre", "BufNewFile" },
+
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "help",
+          "alpha",
+          "hyper",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+        callback = function() vim.b.miniindentscope_disable = true end,
+      })
+    end,
+  },
 }
