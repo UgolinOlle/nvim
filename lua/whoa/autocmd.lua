a244791687b715b9eff0602312d19e3f3eec8b3d
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:42:54 +0700

    style(plugins): format with lua style

--- lua/whoa/autocmd.lua
+++ lua/whoa/autocmd.lua
@@ -2,31 +2,27 @@ local autocmd = vim.api.nvim_create_autocmd
 
 -- On Vim enter, disable tmux
 autocmd("VimEnter", {
-	desc = "Disable tmux",
-	callback = function()
-		vim.cmd("silent !tmux set status off")
-	end,
+  desc = "Disable tmux",
+  callback = function() vim.cmd "silent !tmux set status off" end,
 })
 
 -- On Vim exit, enable tmux
 autocmd("VimLeave", {
-	desc = "Enable tmux",
-	callback = function()
-		vim.cmd("silent !tmux set status on")
-	end,
+  desc = "Enable tmux",
+  callback = function() vim.cmd "silent !tmux set status on" end,
 })
 
 -- Adding 42 norm on C file.
 autocmd("FileType", {
-	pattern = "c",
-	command = "setlocal shiftwidth=4 tabstop=4",
+  pattern = "c",
+  command = "setlocal shiftwidth=4 tabstop=4",
 })
 
 -- Disable some style on terminal.
 autocmd("TermOpen", {
-	desc = "Disable foldcolumn in terminal and signcolumn for terminal.",
-	callback = function()
-		vim.opt.foldcolumn = "0"
-		vim.opt.signcolumn = "no"
-	end,
+  desc = "Disable foldcolumn in terminal and signcolumn for terminal.",
+  callback = function()
+    vim.opt.foldcolumn = "0"
+    vim.opt.signcolumn = "no"
+  end,
 })
