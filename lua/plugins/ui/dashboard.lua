f0ad1c3bfa02ed91ae7ad20f7b74a33544257961
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-13 17:42:04 +0700

    feat(plugins): style format + adding quit shortcut

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -28,37 +28,46 @@ return {
       winbar = true,
     },
 
-    center = {
-      {
-        icon = get_icons("Cube", 1),
-        desc = "Lazy sync / TSUpdate / Mason",
-        key = "s",
-        action = "WhoaUpdateAll",
+    config = {
+      center = {
+        {
+          icon = get_icons("Cube", 1),
+          desc = "Lazy sync / TSUpdate / Mason",
+          key = "s",
+          action = "WhoaUpdateAll",
+        },
+        {
+          icon = get_icons("File", 1),
+          desc = "Find File",
+          key = "f",
+          action = "Telescope find_files",
+        },
+        {
+          icon = get_icons("FileOld", 1),
+          desc = "Recently Used Files",
+          key = "r",
+          action = "Telescope oldfiles",
+        },
+        {
+          icon = get_icons("Search", 1),
+          desc = "Find Word",
+          key = "w",
+          action = "Telescope live_grep",
+        },
+        {
+          icon = get_icons("Terminal", 1),
+          desc = "Open Terminal",
+          key = "t",
+          action = "ToggleTerm",
+        },
+        {
+          icon = get_icons("BufferClose", 1),
+          desc = "Close Whoa IDE",
+          key = "q",
+          action = "q",
+        },
       },
-      {
-        icon = get_icons("File", 1),
-        desc = "Find File",
-        key = "f",
-        action = "Telescope find_files",
-      },
-      {
-        icon = get_icons("File", 1),
-        desc = "Recently Used Files",
-        key = "r",
-        action = "Telescope oldfiles",
-      },
-      {
-        icon = get_icons("File", 1),
-        desc = "Find Word",
-        key = "w",
-        action = "Telescope live_grep",
-      },
-      {
-        icon = get_icons("Terminal", 1),
-        desc = "Open Terminal",
-        key = "t",
-        action = "ToggleTerm",
-      },
+      footer = {},
     },
   },
 }
