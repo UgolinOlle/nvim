e45def4a09a7005a38967c279264c2284edba3f7
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-26 23:24:53 +0700

    feat(plugins): configure notify

---
+++ lua/plugins/ui/notify.lua
@@ -0,0 +1,18 @@
+return {
+  "rcarriga/nvim-notify",
+
+  event = "BufReadPre",
+
+  opts = {
+    timeout = 100,
+    stages = "fade_in_slide_out",
+    top_down = true,
+    render = "simple",
+    fps = 120
+  },
+
+  keys = {
+    { "<LEADER>nn", "<CMD>lua require('notify').dismiss()<CR>", { noremap = true, silent = true, desc = "Dismiss notifications" } },
+    { "<LEADER>nl", "<CMD>Notifications<CR>", { noremap = true, silent = true, desc = "List notifications" } },
+  }
+}
