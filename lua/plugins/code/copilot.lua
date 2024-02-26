20f1caedaea79b92d72b525334fb381251587687
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-26 10:03:13 +0700

    feat(plugins): move to official copilot

--- lua/plugins/code/copilot.lua
+++ lua/plugins/code/copilot.lua
@@ -1,7 +1,33 @@
 return {
-  "zbirenbaum/copilot.lua",
-  
-  cmd = "Copilot",
-  
-  event = "InsertEnter",
+	"github/copilot.vim",
+
+	event = "InsertEnter",
+
+	cmd = "Copilot",
+
+	config = function()
+		local active, copilot = pcall(require, "copilot")
+		if not active then
+			return
+		end
+
+		copilot.setup({
+			layout = {
+				position = "bottom",
+				ratio = 0.4,
+			},
+
+			suggestion = {
+				enabled = true,
+
+				keymap = {
+					accept = "<TAB>",
+					accept_line = "<S-TAB>",
+					next = "<C-]>",
+					prev = "<C-[>",
+					dimiss = "<ESC>",
+				},
+			},
+		})
+	end,
 }
