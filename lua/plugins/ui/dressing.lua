fa53806fb48c9c0d39fbcbfccced2b8e7f2f824e
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-27 14:24:34 +0700

    feat(plugins): update config for dressing

--- lua/plugins/ui/dressing.lua
+++ lua/plugins/ui/dressing.lua
@@ -2,4 +2,21 @@ return {
   "stevearc/dressing.nvim",
 
   event = "VeryLazy",
+
+  config = function()
+    local active, dressing = pcall(require, "dressing")
+    if not active then return end
+
+    dressing.setup {
+      select = {
+        telescope = require("telescope.themes").get_dropdown(),
+      },
+
+      input = {
+        insert_only = false,
+        relative = "editor",
+        default_prompt = " ",
+      },
+    }
+  end,
 }
