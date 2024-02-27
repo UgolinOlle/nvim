d1006be834aa53187408717a8cb2e1e0642022fa
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-27 09:48:17 +0700

    style: format

--- lua/plugins/code/linting.lua
+++ lua/plugins/code/linting.lua
@@ -15,16 +15,13 @@ return {
       typescript = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       javascriptreact = { "eslint_d" },
-    },
+    }
 
-    local lint_augroup = vim.api.nvim_create_augroups("lint", { clear = true })
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" },
-    {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
+      callback = function() lint.try_lint() end,
     })
-  end
+  end,
 }
