9ceeca95b275f8951f660749a2c84ef054eaf080
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-27 14:24:10 +0700

    fix(plugins): implement noice for lsp

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,5 +1,5 @@
 -- Import
-local utils = require("whoa.utils")
+local utils = require "whoa.utils"
 local get_icons = utils.get_icons
 
 return {
@@ -13,44 +13,41 @@ return {
   event = "VeryLazy",
 
   opts = {
-    lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true
-      }
-    },
-
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
-      inc_rename= false,
-      lsp_doc_border = false
-    },
-
+    -- Command line configuration
     cmdline = {
-      enable = true,
+      enabled = true,
       view = "cmdline_popup",
       format = {
         cmdline = { pattern = "^:", icon = get_icons("Command", 1), lang = "vim" },
         search_down = { kind = "search", pattern = "^/", icon = get_icons("Search", 1), lang = "regex" },
         search_up = { kind = "search", pattern = "^%?", icon = get_icons("Search", 1), lang = "regex" },
         filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-      }
+      },
     },
 
-    messages = {
-      enabled = true,
+    -- LSP configuration
+    lsp = {
+      progress = {
+        enabled = true,
+      },
     },
 
-    popupmenu = {
-      enabled = true,
+    -- Preset configuration
+    preset = {
+      lsp_doc_border = true,
     },
+  },
 
-    redirect = {
-      view = "popup",
-      filter = { event = "msg_show" },
+  keys = {
+    {
+      "gl",
+      function() require("noice").cmd "last" end,
+      { noremap = true, silent = true, desc = "Show last message" },
     },
-  }
+    {
+      "gm",
+      "<CMD>messages<CR>",
+      { noremap = true, silent = true, desc = "Show messages" },
+    },
+  },
 }
