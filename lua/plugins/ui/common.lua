a12bfa1c99cefad8622a12da1b399c4c27ad32f6
Ugolin Ollé <hello@ugolin-olle.com>
2024-08-09 22:38:51 +0700

    refactor(plugins): remove rainbow-delimiter

--- lua/plugins/ui/common.lua
+++ lua/plugins/ui/common.lua
@@ -25,39 +25,6 @@ return {
     config = function() require("colorizer").setup {} end,
   },
   {
-    "HiPhish/rainbow-delimiters.nvim",
-
-    name = "Rainbow Delimiters",
-
-    config = function()
-      local rainbow_delimiters = require "rainbow-delimiters"
-
-      vim.g.rainbow_delimiters = {
-        strategy = {
-          [""] = rainbow_delimiters.strategy["global"],
-          vim = rainbow_delimiters.strategy["local"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          lua = "rainbow-blocks",
-        },
-        priority = {
-          [""] = 110,
-          lua = 210,
-        },
-        highlight = {
-          "RainbowDelimiterRed",
-          "RainbowDelimiterYellow",
-          "RainbowDelimiterBlue",
-          "RainbowDelimiterOrange",
-          "RainbowDelimiterGreen",
-          "RainbowDelimiterViolet",
-          "RainbowDelimiterCyan",
-        },
-      }
-    end,
-  },
-  {
     "folke/zen-mode.nvim",
 
     name = "Zen Mode",
