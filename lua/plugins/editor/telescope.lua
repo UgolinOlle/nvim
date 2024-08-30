c236fa16c589e45f6d1451a2aa10ee83bde741d6
Ugolin Ollé <hello@ugolin-olle.com>
2024-08-30 18:12:26 +0700

    ✨ Feat(plugins): improve telescope

--- lua/plugins/editor/telescope.lua
+++ lua/plugins/editor/telescope.lua
@@ -4,9 +4,11 @@ return {
   name = "Telescope",
 
   dependencies = {
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-telescope/telescope-node-modules.nvim",
-    "2kabhishek/nerdy.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", name = "Telescope fzf native" },
+    { "nvim-telescope/telescope-file-browser.nvim", name = "Telescope file browser" },
+    { "nvim-telescope/telescope-symbols.nvim", name = "Telescope symbols" },
+    { "nvim-telescope/telescope-node-modules.nvim", name = "Telescope node modules" },
+    { "2kabhishek/nerdy.nvim", name = "Telescope nerdy" },
   },
 
   config = function()
@@ -16,6 +18,7 @@ return {
 
     -- Telescope variables
     local actions = require "telescope.actions"
+    local fb_actions = require("telescope").extensions.file_browser.actions
 
     -- Utils variables
     local utils = require "whoa.core.utils"
@@ -69,12 +72,13 @@ return {
           "vendor/*",
         },
         layout_strategy = "horizontal",
+        borderchars = vim.g.border.borderchars,
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
-        prompt_prefix = get_icons("Command", 2), -- Prefix for prompt
-        selection_caret = get_icons("ArrowRight", 2), -- Icon for selection
-        entry_prefix = "  ", -- Prefix for each entry
+        prompt_prefix = get_icons("Command", 2),
+        selection_caret = get_icons("ArrowRight", 2),
+        entry_prefix = "  ",
         mappings = {
           i = {
             ["<C-k>"] = actions.move_selection_previous,
@@ -85,12 +89,46 @@ return {
 
         pickers = {
           find_files = {
+            theme = "dropdown", -- 'ivy', 'dropdown', 'cursor'
+            layout_strategy = "vertical",
+            layout_config = { height = 0.9 },
+            previewer = false,
+            sorting_strategy = "ascending",
+          },
+          buffers = {
+            previewer = false,
             theme = "dropdown",
+            mappings = {
+              n = {
+                ["<C-e>"] = "delete_buffer",
+                ["l"] = "select_default",
+              },
+            },
+            initial_mode = "normal",
           },
           diagnostics = {
             theme = "ivy",
             initial_mode = "normal",
             layout_config = { preview_cutoff = 9999 },
+          },
+        },
+        extensions = {
+
+          file_browser = {
+            dir_icon = "",
+            prompt_path = true,
+            grouped = true,
+            theme = "dropdown",
+            initial_mode = "normal",
+            previewer = false,
+            mappings = {
+              n = {
+                ["o"] = "select_default",
+                ["H"] = fb_actions.toggle_hidden,
+                ["h"] = fb_actions.goto_parent_dir,
+                ["l"] = "select_default",
+              },
+            },
           },
         },
       },
@@ -99,14 +137,20 @@ return {
     telescope.load_extension "fzf"
     telescope.load_extension "node_modules"
     telescope.load_extension "nerdy"
+    telescope.load_extension "file_browser"
   end,
 
   keys = {
     {
+      ";ff",
+      "<CMD>Telescope file_browser path=%:p:h select_buffer=true<CR>",
+      { noremap = true, silent = true, desc = "Find files" },
+    },
+    {
       ";f",
       function()
         local builtin = require "telescope.builtin"
-        builtin.find_files { hidden = true }
+        builtin.find_files()
       end,
       { noremap = true, silent = true, desc = "Find files" },
     },
