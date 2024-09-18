55aa5bcbc3f6a76e4e169efe36fc66c5f5a6b7aa
Ugolin Ollé <hello@ugolin-olle.com>
2024-09-18 14:05:13 +0700

    feat(plugins): adding which-key in float popup

--- lua/plugins/ui/which-key.lua
+++ lua/plugins/ui/which-key.lua
@@ -9,4 +9,42 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
+
+  opts = {
+    plugins = {
+      marks = true,
+      registers = true,
+      spelling = {
+        enabled = true,
+        suggestions = 20,
+      },
+      presets = {
+        operators = false,
+        motions = false,
+        text_objects = false,
+        windows = true,
+        nav = true,
+        z = true,
+        g = true,
+      },
+    },
+
+    win = {
+      border = "rounded",
+      relative = "editor",
+      row = vim.o.lines - 10,
+      col = math.floor(vim.o.columns / 2 - 25),
+      padding = { 2, 2, 2, 2 },
+    },
+
+    layout = {
+      height = { min = 4, max = 25 },
+      width = { min = 20, max = 50 },
+      spacing = 3,
+      align = "center",
+    },
+
+    show_help = true,
+    triggers = { "<leader>" },
+  },
 }
