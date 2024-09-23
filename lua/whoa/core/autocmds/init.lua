880e48139b62eb55f0a32d32be92da6fab1a3dd0
Ugolin Ollé <hello@ugolin-olle.com>
2024-09-23 19:09:58 +0700

    refactor(core): remove unused autocmd for 42 school

--- lua/whoa/core/autocmds/init.lua
+++ lua/whoa/core/autocmds/init.lua
@@ -8,24 +8,6 @@
 --- Variables
 local autocmd = vim.api.nvim_create_autocmd
 
---- Adding 42 norm on C file.
-autocmd("FileType", {
-  pattern = "c",
-  callback = function()
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.tabstop = 4
-  end,
-})
-
---- Adding 42 norm on H file.
-autocmd("FileType", {
-  pattern = "cpp",
-  callback = function()
-    vim.opt_local.shiftwidth = 4
-    vim.opt_local.tabstop = 4
-  end,
-})
-
 --- Disable some style on terminal.
 autocmd("TermOpen", {
   desc = "Disable foldcolumn in terminal and signcolumn for terminal.",
