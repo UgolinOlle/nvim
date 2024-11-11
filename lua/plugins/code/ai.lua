9639826431f5d8fb07e042e3c6f21f51757dbfee
Ugolin Ollé <hello@ugolin-olle.com>
2024-11-11 15:32:56 +0700

    feat(core): support ollama]

--- lua/plugins/code/ai.lua
+++ lua/plugins/code/ai.lua
@@ -8,7 +8,32 @@ return {
   version = false,
 
   opts = {
-    provider = "copilot",
+    provider = "ollama",
+    vendors = {
+      ["ollama"] = {
+        ["local"] = true,
+        endpoint = "127.0.0.1:11434/v1",
+        model = "codegemma",
+        parse_curl_args = function(opts, code_opts)
+          return {
+            url = opts.endpoint .. "/chat/completions",
+            headers = {
+              ["Accept"] = "application/json",
+              ["Content-Type"] = "application/json",
+            },
+            body = {
+              model = opts.model,
+              messages = require("avante.providers").copilot.parse_messages(code_opts),
+              max_tokens = 2048,
+              stream = true,
+            },
+          }
+        end,
+        parse_response = function(data_stream, event_state, opts)
+          require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
+        end,
+      },
+    },
   },
 
   build = "make",
