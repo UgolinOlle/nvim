c2382864e6956cdd42bfcd5de7f3858c51514baf
Ugolin O. <hello@ugolin-olle.com>
2025-01-10 15:43:13 +0700

    feat(git): adding git blame & diff view

--- lua/plugins/code/git.lua
+++ lua/plugins/code/git.lua
@@ -40,4 +40,28 @@ return {
       )
     end,
   },
+  {
+    "f-person/git-blame.nvim",
+
+    name = "Git Blame",
+
+    event = "VeryLazy",
+
+    opts = {
+      enabled = true,
+      message_template = " <summary> • <date> • <author> • <<sha>>",
+      date_format = "%m-%d-%Y %H:%M:%S",
+      virtual_text_column = 1,
+    },
+  },
+  {
+    "sindrets/diffview.nvim",
+
+    name = "Git Diff View",
+
+    keys = {
+      { "<LEADER>gd", "<CMD>DiffviewOpen<CR>",  { noremap = true, silent = true, desc = "Open diff view." } },
+      { "<LEADER>gD", "<CMD>DiffviewClose<CR>", { noremap = true, silent = true, desc = "Close diff view." } },
+    }
+  }
 }
