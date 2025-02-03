a112d71fe715e662e5aa4e24c752b6cdb1fbd3d3
Ugolin O. <hello@ugolin-olle.com>
2025-02-03 12:51:46 +0700

    fix(linter): remove unused linter

--- lua/plugins/code/linting.lua
+++ lua/plugins/code/linting.lua
@@ -18,45 +18,26 @@ return {
       typescript = { "eslint" },
       typescriptreact = { "eslint" },
       javascriptreact = { "eslint" },
-      php = { "phpcs" },
-      twig = { "twigcs", "stylelint", "eslint" },
-      c = { "norminette" },
-      cpp = { "norminette" },
       markdown = { "eslint" },
-      python = { "flake8" },
       swift = { "swiftlint" },
-    }
-
-    lint.linters.norminette = {
-      cmd = "norminette",
-      stdin = false,
-      args = {},
-      stream = "stdout",
-      ignore_exitcode = true,
-      parser = function(output, _)
-        local diagnostics = {}
-        local lines = vim.split(output, "\n")
-        for _, line in ipairs(lines) do
-          local filename, lnum, col, message = string.match(line, "(.-):(%d+):(%d+): (.*)")
-          if filename and lnum and col and message then
-            table.insert(diagnostics, {
-              lnum = tonumber(lnum) - 1,
-              col = tonumber(col) - 1,
-              message = message,
-              severity = vim.diagnostic.severity.ERROR,
-              source = "norminette",
-            })
-          end
-        end
-        return diagnostics
-      end,
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
     vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
-      callback = function() lint.try_lint() end,
+      callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        print("Linting triggered for buffer:", bufnr)
+
+        local ft = vim.bo[bufnr].filetype
+        print("Filetype:", ft)
+
+        local linters = require("lint").linters_by_ft[ft]
+        print("Linters:", vim.inspect(linters))
+
+        require("lint").try_lint()
+      end,
     })
   end,
 }
